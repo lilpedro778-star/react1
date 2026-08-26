8cd94565dd0f1569fbea52adf832df5b9b4d6dce
lilpedro778-star <lilpedro778@gmail.com>
2026-08-26 16:22:44 -0300

    Sincronizando projeto

---
+++ AULA 2/29-07/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import h3 from './h3.jpg'
+function App() {
+  return (
+    <div className="App">
+      <h1>Pedro lima</h1>
+      <img src={h3}alt="meme"></img>
+    <i class="fa-solid fa-person-praying"></i>
+    
+    </div>
+  );
+}
+
+export default App;
+
---
+++ AULA4/aula4/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>pedrinho</h1>
+    <img src="/assets/image/foto.jpg" alt="" ></img>
+    
+    </div>
+  );
+}
+
+export default App;
