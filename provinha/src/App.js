b668922cd28f2df3a061813fb3310f8c0bf930c2
lilpedro778-star <lilpedro778@gmail.com>
2026-08-26 18:18:02 -0300

    Remove node_modules da provinha e ajusta quebras de linha

---
+++ provinha/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import foto from './/OIP.webp'
+function App() {
+  return (
+    <div className="App">
+      <h1>Pedro pereira info d </h1>
+      <img src={foto} alt="" />
+      
+  
+    </div>
+  );
+}
+
+export default App;
